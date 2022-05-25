a5538e455f08769839d1d155f32069fa10cfb90d
VANESSA CLARO NASCIMENTO <v.savoldi@hotmail.com>
2022-05-25 17:17:00 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Quadrilatero
+        System.out.println("Exercicio quadrilatero");
+        Quadrilatero.area( lado 3);
+        Quadrilatero.area(5d,5d);
+        quadrilatero.area( baseMaior 7, baseMenor 8, altura 9);
+        Quadrilatero.area(5f,5f);
+    }
+}
